6f87ee9b88268813ab12722afc45fa95648ff7a3
velomad <48247308+velomad@users.noreply.github.com>
2020-10-18 23:33:02 +0530

    clean code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const logger = require("morgan");
-const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 require("dotenv").config();
